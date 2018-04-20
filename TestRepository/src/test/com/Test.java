20447fe0dcd5758c010db210129978be5afbf0a7
a.azendour <a.azendour@ITECH-CYBERTRON.itech.itech-progress.com>
2018-04-20 13:44:46 +0200

    Initial checkin

---
+++ TestRepository/src/test/com/Test.java
@@ -0,0 +1,17 @@
+package test.com;
+
+public class Test {
+
+	public Test() {
+		
+	}
+
+	public static void main(String[] args) {
+
+
+		System.err.println("hhhhh");
+		System.out.println("jkjhkjhk");
+		//jjjjj
+	}
+
+}
